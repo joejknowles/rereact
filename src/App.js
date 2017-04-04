bd8eb523b7a8c915f4c8b2cdaaaab3788156edf1
Joseph Knowles <joejknowles@gmail.com>
2017-04-04 16:19:03 +0100

    set up test page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
+import React, { Comp } from './rereact';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class App extends Comp {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{ this.props.title }</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Count: <span>{ this.state.count }</span>
         </p>
+        <ColourSwatch number={ this.state.count } />
       </div>
+    );
+  }
+}
+
+class ColourSwatch extends Comp {
+  render() {
+    const red = this.props.number % 256;
+    return (
+      <div
+        style={{
+          backgroundColor: `rgb(${red}, 0, 0)`,
+          height: '50px',
+          width: '50px',
+          margin: 'auto'
+        }}
+      />
     );
   }
 }
---
+++ src/rereact/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export default React;
+
+export const Comp = Component;
